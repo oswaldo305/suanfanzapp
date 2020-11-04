b2ad5eb35656be6eccf7a84d454532a23b69786d
oswaldo ordoñez <os-w316@hotmail.com>
2020-11-04 16:51:31 -0500

    Fixing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {MainContainerComponent} from './components/main-container/main-container.component';
+import {ChatRoomComponent} from './components/main-container/chat-area/chat-room/chat-room.component';
+import {ChatDefaultPageComponent} from './components/main-container/chat-area/chat-default-page/chat-default-page.component';
+import {LoginComponent} from './components/login/login.component';
+import {ChatGuard} from './guards/chat.guard';
+import { RegisterComponent } from './components/register/register.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MainContainerComponent,
+    children: [
+      {
+        path: 'room/:id',
+        component: ChatRoomComponent
+      },
+      {
+        path: '',
+        component: ChatDefaultPageComponent
+      }
+    ],
+    canActivate: [ChatGuard]
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  }
+  {
+    path: 'register',
+    component: RegisterComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyA-ZpIQJB7HHYP8_92xRUNtK-jbFN1rqdg",
+    authDomain: "suanfanzapp-f7c5c.firebaseapp.com",
+    databaseURL: "https://suanfanzapp-f7c5c.firebaseio.com",
+    projectId: "suanfanzapp-f7c5c",
+    storageBucket: "suanfanzapp-f7c5c.appspot.com",
+    messagingSenderId: "746593695166",
+    appId: "1:746593695166:web:735af170bc419b42ae75ef"
+  }
+};
